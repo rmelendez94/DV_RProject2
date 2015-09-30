0b1f171053d19052f7c3234fcfa7611f60d6f5ac
Rolando Melendez <rolando.melendez@utexas.edu>
2015-09-29 22:54:37 -0500

    Removed the group_by(), it was causing problems. Instead i was able to use mutate() to get the percentiles using ntile(). Update BankEfforts_PD.R and the .Rmd file and knitted the most current version of the .html. 4 total (similar) visualizations.

--- 02 Data Wrangling/BankEffort_PD.R
+++ 02 Data Wrangling/BankEffort_PD.R
@@ -6,20 +6,27 @@ require(ggplot2)
 # tbl_df(df)
 # View(df)
 
-# This shows how many total people had the same duration and past outcome, ex: 2 poeple with past failure and duration of 6 and said no to current term loan
-pd <- df %>% select(POUTCOME, DURATION, Y) %>% group_by(POUTCOME, DURATION, Y) %>% summarise(NumInCategory=n()) %>% arrange(DURATION)
+# This shows the 40th percentile and below for call duration during the Marketing campaign
+pd <- df %>% mutate(DURATIONPERCENT = ntile(DURATION, 100)) %>% arrange(DURATIONPERCENT) %>% select(POUTCOME, DURATION, Y, DURATIONPERCENT) %>% filter(DURATIONPERCENT < 40) 
 #View(pd) # Uncomment to view the results
-ypd <- df %>% select(POUTCOME, DURATION, Y) %>% filter(Y=="yes") 
-#View(ypd) # Uncomment to view the results
-npd <- df %>% select(POUTCOME, DURATION, Y) %>% filter(Y=="no")
+
+# This shows the 90th percentile and above for call duration during the Marketing campaign
+pdn <- df %>% mutate(DURATIONPERCENT = ntile(DURATION, 100)) %>% arrange(DURATIONPERCENT) %>% select(POUTCOME, DURATION, Y, DURATIONPERCENT) %>% filter(DURATIONPERCENT > 80) 
+#View(pdn) 
+
+npd <- df %>% mutate(DURATIONPERCENT = ntile(DURATION, 100)) %>% arrange(DURATIONPERCENT) %>% select(POUTCOME, DURATION, Y, DURATIONPERCENT) %>% filter(Y=="no", DURATIONPERCENT < 40) 
 #View(npd) # Uncomment to view the results
 
-# Create the Point Plot; POUTCOME/DURATION
+#This shows the 90th percentile and above for call duration during the Marketing campaign also filtering only yes
+ypd <- df %>% mutate(DURATIONPERCENT = ntile(DURATION, 100)) %>% arrange(DURATIONPERCENT) %>% select(POUTCOME, DURATION, Y, DURATIONPERCENT) %>% filter(Y=="yes", DURATIONPERCENT > 80) 
+#View(ypd) # Uncomment to view the results
+
+# Create the Point Plot; POUTCOME/DURATION lower percentile
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_discrete() +
-  labs(title='Portuguese Bank Marketing Campaign Effectiveness') +
+  labs(title='Portuguese Bank Marketing Campaign Effectiveness (Percentile < 40th)') +
   labs(x="Duration", y=paste("Past Marketing Effectiveness")) +
   layer(data=pd, 
         mapping=aes(x=as.numeric(as.character(DURATION)), y=as.character(POUTCOME), color=Y), 
@@ -30,19 +37,19 @@ ggplot() +
         position=position_jitter(width=0, height=0.3)
   )
 
-# Create the Point Plot; POUTCOME/DURATION with only yes filtered 
+# Create the Point Plot; POUTCOME/DURATION higher percentile
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_discrete() +
-  labs(title='Term Loan Success') +
+  labs(title='Portuguese Bank Marketing Campaign Effectiveness (Percentile > 80th)') +
   labs(x="Duration", y=paste("Past Marketing Effectiveness")) +
-  layer(data=ypd, 
-        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.character(POUTCOME)), 
+  layer(data=pdn, 
+        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.character(POUTCOME), color=Y), 
         stat="identity",
         stat_params=list(),
         geom="point",
-        geom_params=list(colour="#0033CC", alpha=.55), 
+        geom_params=list(alpha=.8), 
         position=position_jitter(width=0, height=0.3)
   )
 
@@ -51,7 +58,7 @@ ggplot() +
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_discrete() +
-  labs(title='Term Loan Failure') +
+  labs(title='Term Loan Failure (Percentile < 40th)') +
   labs(x="Duration", y=paste("Past Marketing Effectiveness")) +
   layer(data=npd, 
         mapping=aes(x=as.numeric(as.character(DURATION)), y=as.character(POUTCOME)), 
@@ -59,5 +66,21 @@ ggplot() +
         stat_params=list(),
         geom="point",
         geom_params=list(colour="#CC0000", alpha=.55), 
+        position=position_jitter(width=0, height=0.3)
+  )
+
+# Create the Point Plot; POUTCOME/DURATION with only yes filtered 
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_discrete() +
+  labs(title='Term Loan Success (Percentile > 80th)') +
+  labs(x="Duration", y=paste("Past Marketing Effectiveness")) +
+  layer(data=ypd, 
+        mapping=aes(x=as.numeric(as.character(DURATION)), y=as.character(POUTCOME)), 
+        stat="identity",
+        stat_params=list(),
+        geom="point",
+        geom_params=list(colour="#0033CC", alpha=.55), 
         position=position_jitter(width=0, height=0.3)
   )
