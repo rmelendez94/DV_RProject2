ccc86e6210bceb2b687f9d8c4ff8d2e257158402
Catherine Tu <tucatherine.95@gmail.com>
2015-10-02 01:58:12 -0500

    Finalized visualizations for Customer Demographics data, and created .png files for visualizations.

--- 02 Data Wrangling/Demographics_AHP.R
+++ 02 Data Wrangling/Demographics_AHP.R
@@ -6,24 +6,49 @@ require(ggplot2)
 #tbl_df(df)
 #View(df)
 
-#Not sure how to use group_by yet
-#ahp <- df %>% group_by(HOUSING, LOAN) %>% summarize()
+ahp <- df %>% select(AGE, HOUSING, LOAN, Y) %>% group_by(HOUSING, LOAN, Y) %>% summarise(medianAge = median(AGE), meanAge = mean(AGE), sdAge = sd(AGE), count = n())
 
-ahp <- df %>% select(AGE, HOUSING, LOAN, Y) %>% mutate(HASLOAN = ifelse(HOUSING == "yes" | LOAN == "yes", "yes", ifelse(HOUSING == "unknown" | LOAN == "unknown", "unknown", "no")))
+ahp
+
+#function to relabel facet grid label for histogram
+result_labeller <- function(var, value){
+  value <- as.character(value)
+  if (var=="Y") { 
+    value[value=="yes"] <- "Campaign Successful"
+    value[value=="no"]   <- "Campaign Unsuccessful"
+  }
+  return(value)
+}
 
 #Create the Point Plot; Loan/Age
 ggplot() + 
   coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
+  scale_x_continuous() +
+  scale_fill_discrete(name = "Campaign Result",
+                      breaks = c("yes", "no"),
+                      labels = c("Successful", "Unsuccessful")) +
+  facet_grid(LOAN~HOUSING~Y, labeller = labeller(Y = result_labeller, LOAN = label_both, HOUSING = label_both)) + 
+  labs(title='Age and Campaign Effectiveness Based on Loan History') +
+  labs(x="Age", y="Frequency") +
+  layer(data=df, 
+        mapping=aes(x = AGE, fill = Y),
+        stat="bin",
+        binwidth = 5,
+        geom="histogram")
+
+ggplot() +
+  coord_cartesian() +
+  scale_x_continuous() +
+  scale_fill_discrete(name = "Campaign Result",
+                      breaks = c("yes", "no"),
+                      labels = c("Successful", "Unsuccessful")) +
   facet_grid(LOAN~HOUSING, labeller=label_both) +
-  labs(title='Portuguese Bank Marketing Campaign Effectiveness') +
-  labs(x="Has Other Loan(s)", y=paste("Age")) +
-  layer(data=ahp, 
-        mapping=aes(x=as.character(HASLOAN), y=as.numeric(as.character(AGE)), color=Y, fill = Y), 
-        stat="identity",
-        stat_params=list(),
-        geom="point",
-        geom_params=list(), 
-        position=position_jitterdodge(jitter.width = 0.3)
-  )
+  labs(title ='Density Plot of Age on Campaign Effectiveness') +
+  labs(x="Age", y="Density") + 
+  layer(data = df,
+        mapping = aes(x = AGE, fill=Y),
+        stat = "density",
+        geom = "density",
+        alpha = 0.25)
+
+# relative to those who have personal loans, those who do not have personal loans are more likely to say yes regardless of if they have a housing loan. results may be inconclusive however because there is more data of those who do not have personal loans
